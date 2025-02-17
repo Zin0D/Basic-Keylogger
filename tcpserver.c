071ccb3c2832be7d7519469491cb1ca27129063f
Zin <zinedinemokhefi@gmail.com>
2025-02-17 03:12:28 +0100

    BUFF

--- tcpserver.c
+++ tcpserver.c
@@ -1,7 +1,7 @@
 #include <winsock2.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <Windows.h>
+#include <Windows.h> 
 //Return 0 = no failure, ASM like.
 //Return 1 = Failure.
 //https://learn.microsoft.com/en-us/windows/win32/api/winsock2/ Documentation.
@@ -14,13 +14,7 @@
 #define ERROR_MESSAGE "HTTP/1.1 404 NOT FOUND"
 
 int main(int argc, char *argv[]) {
-    MessageBoxW(0, L"Creating WinSocket", L"NetWorking", MB_OKCANCEL);
-    if (MessageBoxW(0, L"Creating WinSocket", L"NetWorking", MB_OKCANCEL) == ){
-        printf("Aborting...")
-        exit(0);
-    }
-    printf("Creating WinSocket..."); //Sugoma Balls fucking Windows
-    
+
     WSADATA data; //Holds Information about WinSockLib
     SOCKET sock;
     SOCKET check_sock_invalid = INVALID_SOCKET;
@@ -29,6 +23,9 @@ int main(int argc, char *argv[]) {
     int recv_lenght = NETWORKING_BUFFER;
     char request[25] = "GET /index.html HTTP/1.1"; 
     char response[27] = " / This is a test message "; 
+
+    MessageBoxW(0, L"Creating WinSocket", L"NetWorking", MB_OKCANCEL);
+    printf("Creating WinSocket..."); //Sugoma Balls fucking Windows
 
     if (WSAStartup(MAKEWORD(2,2),&data) != 0) { //If WsaStartup returns a 0, the Initialisation failed.
         printf("Failed to initialize. : %d\n", WSAGetLastError()); //Backward Compatibility aswell 
