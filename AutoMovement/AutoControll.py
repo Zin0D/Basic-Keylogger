cca4276c99100f74848c9812eaf340f8bfff3474
Zin <zinedinemokhefi@gmail.com>
2025-04-08 22:57:35 +0200

    Update

--- AutoMovement/AutoControll.py
+++ AutoMovement/AutoControll.py
@@ -16,11 +16,11 @@ mouse = Controller()
 payload = base64.b64encode(b"whoami") #Takes input as byte.
 print(payload.decode())
 
-def sleep(time_to_sleep):
+def sleep(time_to_sleep) -> None:
     time.sleep(time_to_sleep)
     
 
-def start_moving():
+def start_moving() -> None:
     keyboard.press('W')
     keyboard.release('W')
     sleep(3)
@@ -28,16 +28,20 @@ def start_moving():
     keyboard.release('S')
     sleep(3)
 
+
 def on_clicking(time_double:float) -> None:
     print("This may Crash your pc, recommended Time in Seconds: 0.01 at fastest!\n You sure you want to continue?: [YES] [NO] ")
-    if (input().lower() == "yes"):
+    if (input().lower() == "no"):
         exit(0)
+
 
     while True:
         mouse.click(button=Button.left)
         sleep(time_double)
 
-thrad_on = threading.Thread(target=on_clicking, args=())
+
+#thrad_on = threading.Thread(target=on_clicking, args=()) #Ready?
+#thrad_on.start() Fix why Thread not working
 
 finalised_gregg = None
 
@@ -50,7 +54,13 @@ if __name__ == "__main__":
     #print("Staring AutoPressing.")
     
     try:
-        subprocess.run(print(cmd().decode()))
+        on_clicking(0.01)
+    except KeyboardInterrupt:
+        print("Stopped Clicking.")
+
+        
+    try:
+        subprocess.run(cmd().decode())
     except Exception as cooked:
         None
     print("Done.")
